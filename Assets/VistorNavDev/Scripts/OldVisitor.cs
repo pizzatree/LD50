77d06b15b5c278824201cbe866202354a426a634
greg brandt <59422589+BradleyGBrandt@users.noreply.github.com>
2022-04-02 16:28:14 -0700

    Added Terrain progression. Visitors, Barricades, Benches, Pan to Visitor

---
+++ Assets/VistorNavDev/Scripts/OldVisitor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class OldVisitor : Visitor
+{
+    protected override void InteractWithParkBench(ParkBench parkBench)
+    {
+        StartCoroutine(SitAtBench(parkBench));
+    }
+}
---
+++ Assets/VistorNavDev/Scripts/ParkBench.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ParkBench : MonoBehaviour
+{
+    [SerializeField] Transform _visitorAnimationStartSpot;
+    public bool Occupied;
+    
+    public Transform VisitorAnimationStartSpot { get => _visitorAnimationStartSpot; }
+}
